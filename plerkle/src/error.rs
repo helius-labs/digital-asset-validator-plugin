895ecde2ad3d03eea5c9e4e00402c35eac560b47
vovkman <liam.vovk@gmail.com>
2023-02-18 16:58:00 -0800

    update err template

--- plerkle/src/error.rs
+++ plerkle/src/error.rs
@@ -2,21 +2,21 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum PlerkleError {
-    #[error("General Plugin Config Error ({msg})")]
+    #[error("General Plugin Config Error {{msg}}")]
     GeneralPluginConfigError { msg: String },
 
-    #[error("Error connecting to the backend data store. Error message: ({msg})")]
+    #[error("Error connecting to the backend data store. Error message: {{msg}}")]
     DataStoreConnectionError { msg: String },
 
-    #[error("Error preparing data store schema. Error message: ({msg})")]
+    #[error("Error preparing data store schema. Error message: {{msg}}")]
     DataSchemaError { msg: String },
 
-    #[error("Error preparing data store schema. Error message: ({msg})")]
+    #[error("Error preparing data store schema. Error message: {{msg}}")]
     ConfigurationError { msg: String },
 
     #[error("Malformed Anchor Event")]
     EventError {},
 
-    #[error("Unable to Send Event to Stream ({msg})")]
+    #[error("Unable to Send Event to Stream {{msg}}")]
     EventStreamError { msg: String },
 }
