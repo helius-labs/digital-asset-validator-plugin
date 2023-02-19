9ccec0d89769fe2ec58bacef3a6a5ae862adca9d
vovkman <liam.vovk@gmail.com>
2023-02-18 17:23:30 -0800

    revert

--- plerkle_messenger/src/error.rs
+++ plerkle_messenger/src/error.rs
@@ -2,21 +2,21 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum MessengerError {
-    #[error("Missing or invalid configuration: {{msg}}")]
+    #[error("Missing or invalid configuration: ({msg})")]
     ConfigurationError { msg: String },
 
-    #[error("Error creating connection: {{msg}}")]
+    #[error("Error creating connection: ({msg})")]
     ConnectionError { msg: String },
 
-    #[error("Error sending data: {{msg}}")]
+    #[error("Error sending data: ({msg})")]
     SendError { msg: String },
 
-    #[error("Error receiving data: {{msg}}")]
+    #[error("Error receiving data: ({msg})")]
     ReceiveError { msg: String },
 
-    #[error("Error ACKing message: {{msg}}")]
+    #[error("Error ACKing message: ({msg})")]
     AckError { msg: String },
 
-    #[error("Error autoclaiming message: {{msg}}")]
+    #[error("Error autoclaiming message: ({msg})")]
     AutoclaimError { msg: String },
 }
