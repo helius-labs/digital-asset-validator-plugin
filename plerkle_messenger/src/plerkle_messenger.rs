627c1b96b1b002d2f0259d65d7acc44808ba864d
Tahsin Tunan <tahsintunan@gmail.com>
2023-08-11 23:54:08 +0600

    add second stream

--- plerkle_messenger/src/plerkle_messenger.rs
+++ plerkle_messenger/src/plerkle_messenger.rs
@@ -15,6 +15,8 @@ pub const ACCOUNT_STREAM: &str = "ACC";
 pub const SLOT_STREAM: &str = "SLT";
 pub const TRANSACTION_STREAM: &str = "TXN";
 pub const BLOCK_STREAM: &str = "BLK";
+pub const ACC_BACKFILL: &str = "ACC_BACKFILL";
+pub const TXN_BACKFILL: &str = "TXN_BACKFILL";
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct RecvData {
@@ -56,7 +58,11 @@ pub trait Messenger: Sync + Send {
     async fn add_stream(&mut self, stream_key: &'static str) -> Result<(), MessengerError>;
     async fn set_buffer_size(&mut self, stream_key: &'static str, max_buffer_size: usize);
     async fn send(&mut self, stream_key: &'static str, bytes: &[u8]) -> Result<(), MessengerError>;
-    async fn recv(&mut self, stream_key: &'static str, consumption_type: ConsumptionType) -> Result<Vec<RecvData>, MessengerError>;
+    async fn recv(
+        &mut self,
+        stream_key: &'static str,
+        consumption_type: ConsumptionType,
+    ) -> Result<Vec<RecvData>, MessengerError>;
     async fn stream_size(&mut self, stream_key: &'static str) -> Result<u64, MessengerError>;
 
     // Ack-ing messages is made a bit awkward by the current interface layout because
